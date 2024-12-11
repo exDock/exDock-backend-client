643a3e97c1d41e3b809ca70d055525f32eb8dda9
Kevin Kruijthof <k.kruijthof@student.tudelft.nl>
2024-12-11 18:33:48 +0100

    Added the sidebar to the template

--- lib/template.dart
+++ lib/template.dart
@@ -1,36 +1,77 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: SideBar()));
+  runApp(const MaterialApp(home: SideBar()));
 }
 
 class SideBar extends StatelessWidget {
   const SideBar({super.key});
 
+  void redirect(int index) {}
+
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      backgroundColor: Color(0xFF2A9D8F),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    int selectedIndex = 0;
+    NavigationRailLabelType labelType = NavigationRailLabelType.all;
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFFFFF),
+      body: Row(
         children: [
-          TextButton.icon(
-            onPressed: () {},
-            label: Text('Home'),
-            icon: Icon(Icons.home),
-          ),
-          TextButton.icon(
-            onPressed: () {},
-            label: Text('Sales'),
-            icon: Icon(Icons.attach_money),
-          ),
-          TextButton.icon(
-            onPressed: () {},
-            label: Text('Catalog'),
-            icon: Icon(Icons.inventory),
-          )
+          NavigationRail(
+              backgroundColor: const Color(0xFFFFFFFF),
+              selectedIndex: selectedIndex,
+              labelType: labelType,
+              leading: Container(
+                  margin: const EdgeInsets.only(left: 20.0),
+                  child: const Image(
+                    image: AssetImage("assets/ExDockLogo.png"),
+                    height: 100,
+                    width: 100,
+                  )),
+              destinations: const [
+                NavigationRailDestination(
+                  label: Text('HOME'),
+                  icon: Icon(Icons.home),
+                ),
+                NavigationRailDestination(
+                  label: Text('SALES'),
+                  icon: Icon(Icons.attach_money),
+                ),
+                NavigationRailDestination(
+                  label: Text('CATALOG'),
+                  icon: Icon(Icons.inventory),
+                ),
+                NavigationRailDestination(
+                  label: Text('CUSTOMERS'),
+                  icon: Icon(Icons.person),
+                ),
+                NavigationRailDestination(
+                  label: Text('MARKETING'),
+                  icon: Icon(Icons.campaign),
+                ),
+                NavigationRailDestination(
+                  label: Text('CONTENT'),
+                  icon: Icon(Icons.source),
+                ),
+                NavigationRailDestination(
+                  label: Text('REPORTS'),
+                  icon: Icon(Icons.flag),
+                ),
+                NavigationRailDestination(
+                  label: Text('STORES'),
+                  icon: Icon(Icons.store),
+                ),
+                NavigationRailDestination(
+                  label: Text('SYSTEM'),
+                  icon: Icon(Icons.settings),
+                ),
+              ])
         ],
       ),
     );
   }
 }
+
+// children: [
+//
+// ],

9fe15fba5e69fe62d2c6ccaf0c266f8467cbbe18
Kevin Kruijthof <kev17062004@gmail.com>
2024-12-12 14:03:51 +0100

    Added small view label

--- lib/template.dart
+++ lib/template.dart
@@ -3,13 +3,15 @@ import 'package:flutter_breadcrumb/flutter_breadcrumb.dart';
 
 void main() {
   runApp(const Template(
+    viewType: "global",
     child: SizedBox(),
   ));
 }
 
 class Template extends StatelessWidget {
   final Widget child;
-  const Template({super.key, required this.child});
+  final String viewType;
+  const Template({super.key, required this.child, required this.viewType});
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +53,7 @@ class Template extends StatelessWidget {
                   ],
                 ))),
         backgroundColor: const Color(0xFFFFFFFF),
-        body: SideBar(child: child),
+        body: SideBar(viewType: viewType, child: child),
       ),
     );
   }
@@ -59,7 +61,8 @@ class Template extends StatelessWidget {
 
 class SideBar extends StatelessWidget {
   final Widget child;
-  const SideBar({super.key, required this.child});
+  final String viewType;
+  const SideBar({super.key, required this.child, required this.viewType});
 
   void redirect(int index) {}
 
@@ -68,6 +71,7 @@ class SideBar extends StatelessWidget {
     int selectedIndex = 0;
     NavigationRailLabelType labelType = NavigationRailLabelType.all;
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Padding(padding: EdgeInsets.only(left: 20)),
         Container(
@@ -122,6 +126,24 @@ class SideBar extends StatelessWidget {
                     icon: Icon(Icons.settings),
                   ),
                 ])),
+        Container(
+          padding:
+              const EdgeInsets.only(left: 20.0, right: 20, top: 4, bottom: 4),
+          decoration: BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(color: Colors.grey.shade200),
+                  right: BorderSide(color: Colors.grey.shade200)),
+              borderRadius:
+                  const BorderRadius.only(bottomRight: Radius.circular(10.0)),
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 4.0,
+                    offset: const Offset(0, 4),
+                    color: Colors.grey.shade300),
+                const BoxShadow(offset: Offset(0, 0), color: Colors.white)
+              ]),
+          child: Text(viewType),
+        ),
         child
       ],
     );

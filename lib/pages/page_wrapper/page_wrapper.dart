9eb09007da4f898a750f448c74e85e8d78a12a68
Rens Pols <rens@cepols.nl>
2024-12-18 23:46:23 +0100

    Deprecated page_wrapper.dart

--- lib/pages/page_wrapper/page_wrapper.dart
+++ lib/pages/page_wrapper/page_wrapper.dart
@@ -2,6 +2,7 @@ import 'package:exdock_backend_client/pages/page_wrapper/side_bar/side_bar.dart'
 import 'package:exdock_backend_client/pages/page_wrapper/top_bar/top_bar.dart';
 import 'package:flutter/material.dart';
 
+@Deprecated("Is implemented in the Router for efficiency")
 class MainPageWrapper extends StatefulWidget {
   const MainPageWrapper({super.key});
 

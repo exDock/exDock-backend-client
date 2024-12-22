d4573c9c3c11be4eafbf80f355409805e9c49161
Rens Pols <rens@cepols.nl>
2024-12-22 17:46:37 +0100

    Made page_name.dart use the current path

--- lib/pages/page_wrapper/top_bar/page_name.dart
+++ lib/pages/page_wrapper/top_bar/page_name.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 
 class PageName extends StatelessWidget {
-  const PageName({super.key, required this.pageName, this.breadCrumbs});
+  const PageName({super.key});
 
-  final String pageName;
-  final String? breadCrumbs;
+  String capitalise(String input) {
+    return input[0].toUpperCase() + input.substring(1).toLowerCase();
+  }
 
   @override
   Widget build(BuildContext context) {
+    List<String> breadCrumbsList = Uri.base.path.split("/");
+    breadCrumbsList.removeAt(0);
+    breadCrumbsList.removeLast();
+    String breadCrumbs = breadCrumbsList.join(" > ").replaceAll("-", " ");
+
     Widget pageNameWidget = Text(
-      pageName,
+      capitalise(Uri.base.path.split("/").last).replaceAll("-", " "),
       style: Theme.of(context).textTheme.headlineLarge,
     );
-    if (breadCrumbs != null) {
+    if (breadCrumbs != "") {
       return Stack(
-        // crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            breadCrumbs!,
+            breadCrumbs,
             style: Theme.of(context).textTheme.labelMedium,
           ),
           Align(

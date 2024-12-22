83b3582d996940b8f7da6a7fac073e2a004d3d59
Rens Pols <rens@cepols.nl>
2024-12-22 15:41:16 +0100

    Put Home in the route

--- lib/router/routes/home.dart
+++ lib/router/routes/home.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dart';
+import 'package:exdock_backend_client/pages/home/home.dart';
 import 'package:go_router/go_router.dart';
 
 List<GoRoute> getHomeRoutes() {
   return [
-    GoRoute(path: '/home', builder: (context, state) => const Placeholder()),
+    GoRoute(path: '/home', builder: (context, state) => Home()),
   ];
 }

47628303d48b0af7230e8b10451745cb084d3a9b
Kevin Kruijthof <kev17062004@gmail.com>
2025-01-04 09:42:05 +0100

    Changed the router to correctly load the page

--- lib/router/routes/products.dart
+++ lib/router/routes/products.dart
@@ -1,11 +1,12 @@
-import 'package:exdock_backend_client/pages/products/top_bar/product_search.dart';
 import 'package:go_router/go_router.dart';
+
+import '../../pages/products/product.dart';
 
 List<GoRoute> getProductsRoutes() {
   return [
     GoRoute(
       path: '/products',
-      builder: (context, state) => const ProductSearch(),
+      builder: (context, state) => Product(),
     ),
   ];
 }

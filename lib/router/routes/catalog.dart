37c5fffe1bbf6eb3f9ed926120d2dbcbab2c5a38
Kevin Kruijthof <kev17062004@gmail.com>
2025-01-11 10:54:37 +0100

    Refactored route

--- lib/router/routes/catalog.dart
+++ lib/router/routes/catalog.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../pages/catalog/products/product.dart';
+
 List<GoRoute> getCatalogRoutes() {
   return [
     GoRoute(
       path: '/catalog',
       builder: (context, state) => const Placeholder(),
+    ),
+    GoRoute(
+      path: '/catalog/products',
+      builder: (context, state) => Product(),
     ),
   ];
 }

62865fb95826671b085afced236b0d52a48bc790
Rens Pols <rens@cepols.nl>
2025-01-13 14:09:40 +0100

    Initialised images_attribute.dart

---
+++ lib/pages/catalog/category/edit/content/attribute_types/images_attribute.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImagesAttribute extends StatefulWidget {
+  const ImagesAttribute({super.key});
+
+  @override
+  State<ImagesAttribute> createState() => _ImagesAttributeState();
+}
+
+class _ImagesAttributeState extends State<ImagesAttribute> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}

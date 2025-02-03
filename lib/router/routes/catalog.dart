e40ccbdceadc8eb4b5f8f72cc68c14bc5b340162
Kevin Kruijthof <kev17062004@gmail.com>
2025-02-03 21:36:57 +0100

    Changed routes to include new URL

--- lib/router/routes/catalog.dart
+++ lib/router/routes/catalog.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../pages/catalog/product/info/product_info.dart';
+import '../../pages/catalog/product/new/new_product.dart';
 
 List<GoRoute> getCatalogRoutes() {
   return [
@@ -20,11 +21,15 @@ List<GoRoute> getCatalogRoutes() {
       builder: (context, state) => Category(),
     ),
     GoRoute(
-      path: '/catalog/product/:id',
+      path: '/catalog/product/info/:id',
       builder: (context, state) {
         final id = state.pathParameters['id'];
         return ProductInfo();
       },
     ),
+    GoRoute(
+      path: "/catalog/product/new",
+      builder: (context, state) => NewProduct(),
+    )
   ];
 }

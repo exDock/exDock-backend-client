c654b7d993fa3e3d8b0e66c2d82e8620f7a9e30f
Rens Pols <rens@cepols.nl>
2025-02-12 21:59:02 +0100

    Created ExdockCard

---
+++ lib/widgets/exdock_card.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ExdockCard extends StatelessWidget {
+  const ExdockCard({
+    super.key,
+    this.width,
+    this.height,
+    this.child,
+  });
+
+  final double? width;
+  final double? height;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: Card(
+        color: Theme.of(context).indicatorColor,
+        elevation: 4,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: child,
+      ),
+    );
+  }
+}

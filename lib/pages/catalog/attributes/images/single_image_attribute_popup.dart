84c4064c2eb5427b3b2b9b8e919236a124b88535
Rens Pols <rens@cepols.nl>
2025-02-19 21:22:10 +0100

    Added the big popup widget and used it in the image attribute popup

--- lib/pages/catalog/attributes/images/single_image_attribute_popup.dart
+++ lib/pages/catalog/attributes/images/single_image_attribute_popup.dart
@@ -1,4 +1,4 @@
-import 'package:exdock_backend_client/widgets/exdock_close_icon.dart';
+import 'package:exdock_backend_client/widgets/popup/exdock_big_popup.dart';
 import 'package:flutter/material.dart';
 import 'package:kumi_popup_window/kumi_popup_window.dart';
 
@@ -18,43 +18,13 @@ class SingleImageAttributePopup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).indicatorColor,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      height: MediaQuery.of(context).size.height * .7,
-      width: MediaQuery.of(context).size.width * .8,
-      child: Stack(
-        children: [
-          SizedBox(
-            height: double.infinity,
-            width: double.infinity,
-            child: Padding(
-              padding: const EdgeInsets.all(24),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Category image [$scope]",
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                  // TODO: upload new image button
-                  // TODO: search grid with all images that are currently on the server (paged)
-                  // TODO: save button -> calls widget.onNewImage(image) with the url of the selected image
-                ],
-              ),
-            ),
-          ),
-          Positioned(
-            top: 12,
-            right: 12,
-            child: ExdockCloseIcon(onPressed: () {
-              pop.dismiss(context);
-            }),
-          ),
-        ],
-      ),
+    return ExdockBigPopup(
+      pop: pop,
+      title: "Category image [$scope]",
+      child: Placeholder(),
+      // TODO: upload new image button
+      // TODO: search grid with all images that are currently on the server (paged)
+      // TODO: save button -> calls widget.onNewImage(image) with the url of the selected image
     );
   }
 }

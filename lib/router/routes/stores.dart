4d1f252fc98457ee9c8abd91638edf3d9701a2d3
Rens Pols <rens@cepols.nl>
2025-03-03 17:06:34 +0100

    Added the configuration route

--- lib/router/routes/stores.dart
+++ lib/router/routes/stores.dart
@@ -1,3 +1,4 @@
+import 'package:exdock_backend_client/pages/stores/configuration/configuration.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -6,6 +7,10 @@ List<GoRoute> getStoresRoutes() {
     GoRoute(
       path: '/stores',
       builder: (context, state) => const Placeholder(),
+    ),
+    GoRoute(
+      path: '/stores/configuration',
+      builder: (context, state) => const Configuration(),
     ),
   ];
 }

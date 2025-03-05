6c4fd79e7e3baa022a466ce844b12bf3c66209b8
Rens Pols <rens@cepols.nl>
2025-03-05 20:36:06 +0100

    Refactor: Introduce `ExdockTextField` widget and use it in `TextFieldAttribute`
    - Created a new `ExdockTextField` widget for consistent text field styling.
    - Replaced the standard `TextField` with the new `ExdockTextField` in `TextFieldAttribute`.

--- lib/pages/catalog/attributes/text_field_attribute.dart
+++ lib/pages/catalog/attributes/text_field_attribute.dart
@@ -1,5 +1,6 @@
 import 'package:exdock_backend_client/utils/MapNotifier.dart';
 import 'package:exdock_backend_client/widgets/exdock_card.dart';
+import 'package:exdock_backend_client/widgets/input/exdock_text_field.dart';
 import 'package:flutter/material.dart';
 
 class TextFieldAttribute extends StatefulWidget {
@@ -27,36 +28,22 @@ class _TextFieldAttributeState extends State<TextFieldAttribute> {
 
   @override
   Widget build(BuildContext context) {
-    return ExdockCard(
-      height: 56,
-      child: TextField(
-        controller: controller,
-        onChanged: (value) {
-          if (value == widget.attribute["current_attribute_value"]) {
-            widget.changeAttributeMap.removeEntry(
-              widget.attribute["attribute_id"],
-            );
-            return;
-          }
-          widget.changeAttributeMap.addEntry(
+    return ExdockTextField(
+      controller: controller,
+      onChanged: (value) {
+        if (value == widget.attribute["current_attribute_value"]) {
+          widget.changeAttributeMap.removeEntry(
             widget.attribute["attribute_id"],
-            value,
           );
-        },
-        style: TextStyle(fontSize: 14, height: 1.5),
-        decoration: InputDecoration(
-          labelText: widget.attribute["attribute_name"] ??
-              widget.attribute["attribute_id"],
-          labelStyle: Theme.of(context).textTheme.bodyMedium,
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: 24, vertical: 0),
-          border: OutlineInputBorder(
-            borderSide: BorderSide.none,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          fillColor: Colors.blue,
-        ),
-      ),
+          return;
+        }
+        widget.changeAttributeMap.addEntry(
+          widget.attribute["attribute_id"],
+          value,
+        );
+      },
+      labelText: widget.attribute["attribute_name"] ??
+          widget.attribute["attribute_id"],
     );
   }
 }

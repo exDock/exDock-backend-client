0054df677d602eeff101639807a7be348bafaf7c
Rens Pols <rens@cepols.nl>
2025-03-13 20:26:32 +0100

    feat: Add `PagesOverview` page and modify `OverviewPage` components
    -   Created a new `PagesOverview` page to display a table of pages.
    -   Introduced `columnValues` to `OverviewPageRow` to store dynamic data for each row.
    -   Modified `OverviewPageColumn` to prioritize `columnKey` over `name`.
    -   Populated the `PagesOverview` page with mock data.

---
+++ lib/pages/content/pages/pages_overview.dart
@@ -0,0 +1,77 @@
+import 'package:exdock_backend_client/widgets/overview_page/columns/overview_page_column.dart';
+import 'package:exdock_backend_client/widgets/overview_page/overview_page.dart';
+import 'package:exdock_backend_client/widgets/overview_page/row/overview_page_row.dart';
+import 'package:flutter/material.dart';
+
+class PagesOverview extends StatelessWidget {
+  const PagesOverview({super.key});
+
+  Future<List<OverviewPageColumn>> getPagesColumns() async {
+    return [
+      OverviewPageColumn(columnKey: "column_1", name: "Column 1"),
+      OverviewPageColumn(columnKey: "column_2", name: "Column 2"),
+      OverviewPageColumn(columnKey: "column_3", name: "Column 3"),
+      OverviewPageColumn(columnKey: "column_4", name: "Column 4"),
+    ];
+  }
+
+  Future<List<OverviewPageRow>> getPagesRows() async {
+    return [
+      OverviewPageRow(
+        id: 'page_1',
+        name: "Page 1",
+        columnValues: {
+          "column_1": "value_1",
+          "column_2": "value_2",
+          "column_3": "value_3",
+        },
+      ),
+      OverviewPageRow(
+        id: 'page_2',
+        name: "Page 2",
+        columnValues: {
+          "column_1": "value_1",
+          "column_2": "value_2",
+        },
+      ),
+      OverviewPageRow(
+        id: 'page_3',
+        name: "Page 3",
+        columnValues: {
+          "column_1": "value_1",
+          "column_2": "value_2",
+          "column_3": "value_3",
+          "column_4": "value_4",
+          "column_5": "value_5",
+        },
+      )
+    ];
+  }
+
+  Future<Map<String, dynamic>> getPagesOverviewData() async {
+    return {
+      "columns": await getPagesColumns(),
+      "rows": await getPagesRows(),
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getPagesOverviewData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return OverviewPage(
+            columns: snapshot.data!['columns'],
+            rows: snapshot.data!['rows'],
+          );
+        }
+
+        // TODO: replace this with ExdockLoadingPageAnimation once available on the main branch
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}

983403aa6e7926888f353dfb9cef3878242c1cf2
Rens Pols <rens@cepols.nl>
2025-03-13 20:26:32 +0100

    feat: Add `PagesOverview` page and modify `OverviewPage` components
    -   Created a new `PagesOverview` page to display a table of pages.
    -   Introduced `columnValues` to `OverviewPageRow` to store dynamic data for each row.
    -   Modified `OverviewPageColumn` to prioritize `columnKey` over `name`.
    -   Populated the `PagesOverview` page with mock data.

--- lib/widgets/overview_page/columns/overview_page_column.dart
+++ lib/widgets/overview_page/columns/overview_page_column.dart
@@ -1,12 +1,12 @@
 class OverviewPageColumn {
   const OverviewPageColumn({
-    required this.name,
     required this.columnKey,
+    required this.name,
   });
-
-  /// The name of the column
-  final String name;
 
   /// The key of the column
   final String columnKey;
+
+  /// The name of the column
+  final String name;
 }
--- lib/widgets/overview_page/row/overview_page_row.dart
+++ lib/widgets/overview_page/row/overview_page_row.dart
@@ -4,9 +4,15 @@ class OverviewPageRow extends StatelessWidget {
   const OverviewPageRow({
     super.key,
     required this.id,
+    required this.name,
+    required this.columnValues,
   });
 
   final String id;
+  final String name;
+
+  /// {key: value}
+  final Map<String, dynamic> columnValues;
 
   @override
   Widget build(BuildContext context) {

a76f9e01b215e85a2f8ae1782d0a2fbcb1c738f5
Rens Pols <rens@cepols.nl>
2025-03-17 13:18:27 +0100

    Add: Implemented the search bar in `overview_page_header.dart`
    This commit introduces a functional search bar within the `OverviewPageHeader` widget. The search bar allows users to input text, view search suggestions, and submit searches to filter the overview page.  It also introduces a stub `applyKeywordFilter` function that is intended to be completed in the future.

--- lib/widgets/overview_page/overview_page_header.dart
+++ lib/widgets/overview_page/overview_page_header.dart
@@ -4,7 +4,7 @@ import 'package:exdock_backend_client/widgets/overview_page/columns/overview_pag
 import 'package:exdock_backend_client/widgets/overview_page/filters/filter.dart';
 import 'package:flutter/material.dart';
 
-class OverviewPageHeader extends StatelessWidget {
+class OverviewPageHeader extends StatefulWidget {
   const OverviewPageHeader({
     super.key,
     required this.columns,
@@ -15,6 +15,15 @@ class OverviewPageHeader extends StatelessWidget {
   final List<OverviewPageColumn> columns;
   final List<BulkAction> bulkActions;
   final List<Filter> filters;
+
+  @override
+  State<OverviewPageHeader> createState() => _OverviewPageHeaderState();
+}
+
+class _OverviewPageHeaderState extends State<OverviewPageHeader> {
+  void applyKeywordFilter(String searchResult) {
+    // TODO: create or edit the "keyword" filter
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +40,53 @@ class OverviewPageHeader extends StatelessWidget {
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // TODO: search bar
+            SearchAnchor(
+              shrinkWrap: true,
+              viewBackgroundColor: Theme.of(context).indicatorColor,
+              builder: (context, searchController) {
+                return SearchBar(
+                  backgroundColor: WidgetStatePropertyAll(
+                    Theme.of(context).indicatorColor,
+                  ),
+                  controller: searchController,
+                  onTap: () {
+                    searchController.openView();
+                  },
+                  onChanged: (_) {
+                    searchController.openView();
+                  },
+                  onSubmitted: (searchText) {
+                    applyKeywordFilter(searchText);
+                  },
+                  padding: const WidgetStatePropertyAll(EdgeInsets.all(16)),
+                  trailing: const [Icon(Icons.search_rounded)],
+                );
+              },
+              suggestionsBuilder: (context, searchController) {
+                Function() onSelect(String searchText) {
+                  return () {
+                    applyKeywordFilter(searchText);
+                    searchController.closeView(searchText);
+                  };
+                }
+
+                ListTile createListTile(String searchText) {
+                  return ListTile(
+                    title: Text(searchText),
+                    onTap: onSelect(searchText),
+                  );
+                }
+
+                // TODO: get search suggestions
+                return [
+                  createListTile("search result 1"),
+                  createListTile("search result 2"),
+                  createListTile("search result 3"),
+                  createListTile("search result 4"),
+                  createListTile("search result 5"),
+                ];
+              },
+            ),
             Row(
               children: [
                 // TODO: bulk actions

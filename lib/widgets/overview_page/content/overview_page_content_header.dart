1c0ffc7168e164015eddb87bb2d176bd7453ed24
Rens Pols <rens@cepols.nl>
2025-03-20 20:04:46 +0100

    Refactor: Implement `SelectAllCheckbox` and improve ID management
    -   Introduced a `SelectAllCheckbox` widget for selecting all items.
    -   Enhanced `IdSetNotifier` to manage selected IDs efficiently.
    -   Updated `OverviewPageRow` to use `ValueListenableBuilder` for checkbox state.
    -   Modified the `addAllId` method in `IdSetNotifier` to use `allIds` for selected IDs.
    -   Updated logic in the `SelectAllCheckbox` to correctly reflect the selection state.

--- lib/widgets/overview_page/content/overview_page_content_header.dart
+++ lib/widgets/overview_page/content/overview_page_content_header.dart
@@ -2,6 +2,7 @@ import 'package:exdock_backend_client/globals/globals.dart';
 import 'package:exdock_backend_client/utils/id_set_notifier.dart';
 import 'package:exdock_backend_client/widgets/overview_page/content/columns/overview_page_column.dart';
 import 'package:exdock_backend_client/widgets/overview_page/content/columns/overview_page_column_cell.dart';
+import 'package:exdock_backend_client/widgets/overview_page/content/utils/select_all_checkbox.dart';
 import 'package:flutter/material.dart';
 
 class OverviewPageContentHeader extends StatelessWidget {
@@ -26,14 +27,7 @@ class OverviewPageContentHeader extends StatelessWidget {
           const SizedBox(width: 1), // compensate for left border
           SizedBox(
             width: 50,
-            child: Checkbox(
-              value: false,
-              side: BorderSide(
-                color: Theme.of(context).indicatorColor,
-                width: 1.5,
-              ),
-              onChanged: (value) {},
-            ),
+            child: SelectAllCheckbox(selectedIds: selectedIds),
           ),
           const OverviewPageColumnCell(columnName: "ID", width: 75),
           const OverviewPageColumnCell(columnName: "name", width: 100),

f60461aca33afe1d049dbb4a63d97f807fb9655d
Rens Pols <rens@cepols.nl>
2025-03-25 00:10:35 +0100

    feat: Implement base classes and initial implementations for filters
    This commit introduces the base `Filter` class and specific implementations: `RangeFilter` and `StringFilter`.
    - `Filter`: An abstract base class for all filter types, containing the filter's name and key. It contains the `filterParameter` getter to get the parameter to be used in the backend request.
    - `RangeFilter`: Implements a filter for numerical ranges, allowing for specifying `min` and `max` values.
    - `StringFilter`: Implements a filter for string values.

--- lib/widgets/overview_page/filters/filter.dart
+++ lib/widgets/overview_page/filters/filter.dart
@@ -1,8 +1,15 @@
-class Filter {
+abstract class Filter {
   const Filter({
     required this.name,
+    required this.key,
   });
 
   /// The name of the filter
   final String name;
+
+  /// The key of the filter
+  final String key;
+
+  /// Returns the parameter that is added to the request
+  MapEntry<String, dynamic> get filterParameter;
 }
---
+++ lib/widgets/overview_page/filters/range_filter.dart
@@ -0,0 +1,23 @@
+import 'package:exdock_backend_client/widgets/overview_page/filters/filter.dart';
+
+class RangeFilter extends Filter {
+  RangeFilter({
+    required super.name,
+    required super.key,
+    this.min,
+    this.max,
+  });
+
+  num? min;
+  num? max;
+
+  @override
+  MapEntry<String, dynamic> get filterParameter {
+    Map<String, num?> value = {};
+
+    if (min != null) value['min'] = min;
+    if (max != null) value['max'] = max;
+
+    return MapEntry(key, value);
+  }
+}

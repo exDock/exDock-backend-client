289c98aa185edc1538ac1a2956f289948ee16baf
Rens Pols <rens@cepols.nl>
2025-04-17 21:40:03 +0200

    Refactor: Improve page caching logic in `retrieve_overview_page_pages.dart`
    - Initialize `pages` list as mutable.
    - Add logic to return the nearest cached page instead of throwing an error if a non existing page is requested.
    - Add logic to set the cached page instead of always adding it.
    - Only try to cache a page if it is within the `pages` bounds.

--- lib/widgets/overview_page/content/row/retrieve_overview_page_pages.dart
+++ lib/widgets/overview_page/content/row/retrieve_overview_page_pages.dart
@@ -37,18 +37,25 @@ class RetrieveOverviewPagePages {
   final List<OverviewPageColumn>? columns;
   final int cacheForwards;
   final int cacheBackwards;
-  final List<OverviewPagePage?> pages = const [];
+  final List<OverviewPagePage?> pages = [];
 
   Future<OverviewPagePage> getOverviewPagePage(
     int pageNumber, {
     bool strict = true,
   }) async {
-    while (pageNumber >= pages.length) {
-      return pages[pageNumber - 1]!;
-    }
+    if (pages.isNotEmpty) {
+      int returnedPageNumber = pageNumber;
+      while (returnedPageNumber >= pages.length) {
+        returnedPageNumber -= 1;
+      }
 
-    if (pages[pageNumber - 1] != null) {
-      return pages[pageNumber - 1]!;
+      if (returnedPageNumber != pageNumber) {
+        return pages[returnedPageNumber]!;
+      }
+
+      if (pages[pageNumber - 1] != null) {
+        return pages[pageNumber - 1]!;
+      }
     }
 
     List<OverviewPageRow> rows = await getRows(
@@ -70,14 +77,18 @@ class RetrieveOverviewPagePages {
       },
     );
 
-    pages[pageNumber - 1] = overviewPagePage;
+    if (pages.length <= pageNumber) {
+      pages.add(overviewPagePage);
+    } else {
+      pages[pageNumber - 1] = overviewPagePage;
+    }
 
     if (!strict && overviewPagePage.rows.isEmpty && pageNumber > 1) {
       return await getOverviewPagePage(pageNumber - 1, strict: false);
     }
 
     for (int i = 0; i < cacheForwards; i++) {
-      if (pages[pageNumber + i] == null) {
+      if (pages.length > pageNumber + i && pages[pageNumber + i] == null) {
         cachePage(pageNumber + i);
       }
     }

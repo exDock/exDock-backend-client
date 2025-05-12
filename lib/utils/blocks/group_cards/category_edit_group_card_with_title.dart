e7f288fe9f97d7d73c25b6e66c7b98eb49ca4171
Kevin Kruijthof <kev17062004@gmail.com>
2025-05-12 22:35:00 +0200

    ``` Refactor(imports): Move Flutter imports to the top ```

--- lib/utils/blocks/group_cards/category_edit_group_card_with_title.dart
+++ lib/utils/blocks/group_cards/category_edit_group_card_with_title.dart
@@ -1,8 +1,7 @@
 // Flutter imports:
-import 'package:flutter/material.dart';
-
 // Project imports:
 import 'package:exdock_backend_client/utils/blocks/group_cards/category_edit_group_card.dart';
+import 'package:flutter/material.dart';
 
 class CategoryEditGroupCardWithTitle extends StatelessWidget {
   const CategoryEditGroupCardWithTitle({

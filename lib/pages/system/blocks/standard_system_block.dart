4db70781920c16f6a891cf51f59c3ff8e995e9f2
Kevin Kruijthof <kev17062004@gmail.com>
2025-05-24 09:36:46 +0200

    ``` feat(lib/pages/system/blocks): Add StandardSystemBlock widget
    ```

---
+++ lib/pages/system/blocks/standard_system_block.dart
@@ -0,0 +1,65 @@
+// Flutter imports:
+import 'package:exdock_backend_client/pages/catalog/product/info/product_info_card/product_info_card_title.dart';
+import 'package:exdock_backend_client/utils/attributes/generate_attribute.dart';
+import 'package:exdock_backend_client/utils/map_notifier.dart';
+import 'package:flutter/material.dart';
+
+class StandardSystemBlock extends StatefulWidget {
+  const StandardSystemBlock({
+    super.key,
+    required this.block,
+    required this.changeSettingsMap,
+  });
+
+  final MapEntry<String, dynamic> block;
+  final MapNotifier changeSettingsMap;
+
+  @override
+  State<StandardSystemBlock> createState() => _StandardSystemBlockState();
+}
+
+class _StandardSystemBlockState extends State<StandardSystemBlock> {
+  late final ValueNotifier<bool> unsavedChangesNotifier =
+      ValueNotifier<bool>(false);
+
+  List<String> getAttributesList() {
+    return List<String>.from(
+      widget.block.value['attributes'].map((e) => e["attribute_id"]).toList(),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.changeSettingsMap.addListener(() {
+      unsavedChangesNotifier.value = widget.changeSettingsMap.attributeChanged(
+        getAttributesList(),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ProductInfoCardTitle(
+      title: widget.block.key,
+      unsavedChangesNotifier: unsavedChangesNotifier,
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: widget.block.value['settings'].length,
+        itemBuilder: (context, index) {
+          Widget child = GenerateAttribute(
+            attribute: widget.block.value['settings'][index],
+            changeAttributeMap: widget.changeSettingsMap,
+          );
+          if (index != 0) {
+            child = Padding(
+              padding: const EdgeInsets.only(top: 24),
+              child: child,
+            );
+          }
+          return child;
+        },
+      ),
+    );
+  }
+}

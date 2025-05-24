40fdfe6f67a48822f01f02a4673b0ffbbc3e130a
Kevin Kruijthof <kev17062004@gmail.com>
2025-05-24 09:31:20 +0200

    ✅ feat(lib/pages/system/blocks): Add SystemBlock widget for displaying system settings.

---
+++ lib/pages/system/blocks/system_block.dart
@@ -0,0 +1,35 @@
+// Flutter imports:
+import 'package:exdock_backend_client/utils/map_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class SystemBlock extends StatefulWidget {
+  const SystemBlock({
+    super.key,
+    required this.block,
+    required this.changeSettingsMap,
+  });
+
+  final List<Widget> block;
+  final MapNotifier changeSettingsMap;
+
+  @override
+  State<SystemBlock> createState() => _SystemBlockState();
+}
+
+class _SystemBlockState extends State<SystemBlock> {
+  @override
+  Widget build(BuildContext context) {
+    return MasonryGridView.count(
+      padding: const EdgeInsets.all(24),
+      mainAxisSpacing: 24,
+      crossAxisSpacing: 24,
+      shrinkWrap: true,
+      crossAxisCount: 2,
+      itemCount: widget.block.length,
+      itemBuilder: (context, index) {
+        return widget.block[index];
+      },
+    );
+  }
+}

eaf469098a176f4ee9631d3c43eda85eb2b852cf
Rens Pols <rens@cepols.nl>
2025-05-26 16:44:46 +0200

    feat: Introduce UserParameter abstraction and TextUserParameter implementation
    This commit introduces an abstract class `UserParameter` to represent user-configurable parameters. It defines a contract for obtaining user input and storing parameter values.
    
    A concrete implementation, `TextUserParameter`, is also added. This class provides a `TextField` widget for text input and manages storing the input value in the provided `parameterValueStorage` map.

---
+++ lib/utils/user_parameters/text_user_parameter.dart
@@ -0,0 +1,25 @@
+import 'package:exdock_backend_client/utils/user_parameters/user_parameter.dart';
+import 'package:exdock_backend_client/widgets/input/exdock_text_field.dart';
+import 'package:flutter/material.dart';
+
+class TextUserParameter extends UserParameter {
+  TextUserParameter(
+    super.key,
+    super.parameterValueStorage, {
+    this.initialValue,
+  });
+
+  final String? initialValue;
+
+  @override
+  Widget getInputWidget() => ExdockTextField(
+        controller: TextEditingController(text: initialValue),
+        onChanged: (value) {
+          if (value == "") {
+            parameterValueStorage.remove(key);
+            return;
+          }
+          parameterValueStorage[key] = value;
+        },
+      );
+}

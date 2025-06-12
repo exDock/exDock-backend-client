e3b5ab536c6c8da485eeffa0acf02d4d9b10b6ab
Kevin Kruijthof <k.kruijthof@student.tudelft.nl>
2025-06-12 17:11:08 +0200

    feat(startup): Initialize settings and shared preferences
    Initialize shared preferences and load config.json.
    
    - Add shared preferences initialization.
    - Load configuration from assets/config.json.
    - Initialize the Settings object with the config.

--- lib/utils/startup.dart
+++ lib/utils/startup.dart
@@ -2,7 +2,16 @@
 // Project imports:
 import 'package:exdock_backend_client/globals/variables.dart';
 import 'package:exdock_backend_client/utils/settings.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> applicationStartUp() async {
-  settings = Settings();
+  SharedPreferencesWithCacheOptions preferencesWithCacheOptions =
+      const SharedPreferencesWithCacheOptions();
+
+  prefs = await SharedPreferencesWithCache.create(
+    cacheOptions: preferencesWithCacheOptions,
+  );
+  String jsonConfig = await rootBundle.loadString("assets/config.json");
+  settings = Settings(jsonConfig);
 }
